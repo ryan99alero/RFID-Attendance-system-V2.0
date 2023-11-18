5734700f187e1f600702831d1666d0422cd989fc
Ryan99alero <ryan@randgraphics.com>
2023-11-18 01:13:51 -0600

    Updated code for OLED Display

--- ESP32_RFID/ESP32_RFID.ino
+++ ESP32_RFID/ESP32_RFID.ino
@@ -1,199 +1,154 @@
-  /* -----------------------------------------------------------------------------
+/* -----------------------------------------------------------------------------
   - Project: RFID attendance system using ESP32
   - Author:  https://www.youtube.com/ElectronicsTechHaIs
   - Date:  6/03/2020
-   -----------------------------------------------------------------------------
-  This code was created by Electronics Tech channel for 
+  -----------------------------------------------------------------------------
+  This code was created by Electronics Tech channel for
   the RFID attendance project with ESP32.
-   ---------------------------------------------------------------------------*/
+  ---------------------------------------------------------------------------*/
+
 //*******************************libraries********************************
 //ESP32----------------------------
-#include <WiFi.h>
+#include <ETH.h>
 #include <HTTPClient.h>
 #include <time.h>
 //RFID-----------------------------
 #include <SPI.h>
 #include <MFRC522.h>
 //OLED-----------------------------
-#include <Wire.h>
-#include <Adafruit_GFX.h>          //https://github.com/adafruit/Adafruit-GFX-Library
-#include <Adafruit_SSD1306.h>      //https://github.com/adafruit/Adafruit_SSD1306
+#include "LCD_Driver.h"
+#include "GUI_Paint.h"
+#include "image.h"
+
+//#include <Adafruit_GFX.h>
+//#include <Adafruit_SSD1306.h>
 //************************************************************************
 #define SS_PIN  4
 #define RST_PIN 5
-// Declaration for SSD1306 display connected using software I2C pins are(22 SCL, 21 SDA)
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+// Declaration for ST7789V2 1.69inch LCD Module display
+// Old Display INFO connected using software I2C pins are(22 SCL, 21 SDA)
+
+#define SCREEN_WIDTH 240 // OLED display width, in pixels
+#define SCREEN_HEIGHT 280 // OLED display height, in pixels
 #define OLED_RESET     0 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 //************************************************************************
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
 //************************************************************************
-/* Set these to your desired credentials. */
-const char *ssid = "SSID";
-const char *password = "Password";
-const char* device_token  = "Device Token";
+// Ethernet Setup
+#define ETH_TYPE        ETH_PHY_TLK110
+#define ETH_ADDR        31
+#define ETH_MDC_PIN     23
+#define ETH_MDIO_PIN    18
+#define ETH_POWER_PIN   17
+#define ETH_CLK_MODE    ETH_CLOCK_GPIO0_IN
+
+
+static bool eth_connected = false;
+
+void onEvent(arduino_event_id_t event, arduino_event_info_t info)
+{
+  switch (event) {
+    case ARDUINO_EVENT_ETH_START:
+      Serial.println("ETH Started");
+      //set eth hostname here
+      ETH.setHostname("ClockFulfillment");
+      break;
+    case ARDUINO_EVENT_ETH_CONNECTED:
+      Serial.println("ETH Connected");
+      break;
+    case ARDUINO_EVENT_ETH_GOT_IP:
+      Serial.println("ETH Got IP");
+      ETH.printInfo(Serial);
+      eth_connected = true;
+      break;
+    case ARDUINO_EVENT_ETH_LOST_IP:
+      Serial.println("ETH Lost IP");
+      eth_connected = false;
+      break;
+    case ARDUINO_EVENT_ETH_DISCONNECTED:
+      Serial.println("ETH Disconnected");
+      eth_connected = false;
+      break;
+    case ARDUINO_EVENT_ETH_STOP:
+      Serial.println("ETH Stopped");
+      eth_connected = false;
+      break;
+    default:
+      break;
+  }
+}
+
+void testClient(const char * host, uint16_t port)
+{
+  Serial.print("\nconnecting to ");
+  Serial.println(host);
+
+  WiFiClient client;
+  if (!client.connect(host, port)) {
+    Serial.println("connection failed");
+    return;
+  }
+  client.printf("GET / HTTP/1.1\r\nHost: %s\r\n\r\n", host);
+  while (client.connected() && !client.available());
+  while (client.available()) {
+    Serial.write(client.read());
+  }
+
+  Serial.println("closing connection\n");
+  client.stop();
+}
 //************************************************************************
-int timezone = x * 3600;   //Replace "x" your timezone.
-int time_dst = 0;
+// Ethernet Setup Stop
+//************************************************************************
+int timezone = -6 * time_dst;   // Timezone for Wichita, Kansas
+int time_dst = 3600;
 String getData, Link;
 String OldCardID = "";
 unsigned long previousMillis1 = 0;
 unsigned long previousMillis2 = 0;
-String URL = "http://YourComputerIP/rfidattendance/getdata.php"; //computer IP or the server domain
+String URL = "http://192.168.29.35/getdata.php"; // Server domain or IP
 //*************************Biometric Icons*********************************
-#define Wifi_start_width 54
-#define Wifi_start_height 49
-const uint8_t PROGMEM Wifi_start_bits[] = {
-  0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x1f,0xf0,0x00,0x00,0x00
-,0x00,0x03,0xff,0xff,0x80,0x00,0x00
-,0x00,0x1f,0xf0,0x1f,0xf0,0x00,0x00
-,0x00,0x7e,0x00,0x00,0xfc,0x00,0x00
-,0x01,0xf0,0x00,0x00,0x1f,0x00,0x00
-,0x03,0xc0,0x00,0x00,0x07,0xc0,0x00
-,0x0f,0x00,0x00,0x00,0x01,0xe0,0x00
-,0x1c,0x00,0x00,0x00,0x00,0x70,0x00
-,0x38,0x00,0x07,0xc0,0x00,0x38,0x00
-,0x70,0x00,0xff,0xfe,0x00,0x1e,0x00
-,0xe0,0x03,0xfc,0x7f,0xc0,0x0e,0x00
-,0x00,0x1f,0x80,0x03,0xf0,0x00,0x00
-,0x00,0x3c,0x00,0x00,0x78,0x00,0x00
-,0x00,0xf0,0x00,0x00,0x1c,0x00,0x00
-,0x01,0xe0,0x00,0x00,0x0c,0x00,0x00
-,0x03,0x80,0x00,0x00,0x00,0x00,0x00
-,0x03,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x3f,0xf8,0x07,0x1e,0x00
-,0x00,0x00,0xff,0xfe,0x1f,0xbf,0x80
-,0x00,0x03,0xe0,0x04,0x7f,0xff,0xc0
-,0x00,0x07,0x80,0x00,0xff,0xff,0xe0
-,0x00,0x0e,0x00,0x00,0xff,0xff,0xe0
-,0x00,0x0c,0x00,0x00,0x7f,0xff,0xc0
-,0x00,0x00,0x00,0x00,0xfe,0x07,0xe0
-,0x00,0x00,0x00,0x03,0xf8,0x03,0xf8
-,0x00,0x00,0x07,0xe7,0xf9,0xf1,0xfc
-,0x00,0x00,0x1f,0xe7,0xf1,0xf9,0xfc
-,0x00,0x00,0x1f,0xe7,0xf3,0xf9,0xfc
-,0x00,0x00,0x3f,0xe7,0xf3,0xf9,0xfc
-,0x00,0x00,0x3f,0xe7,0xf1,0xf1,0xfc
-,0x00,0x00,0x3f,0xe3,0xf8,0xe3,0xfc
-,0x00,0x00,0x3f,0xf3,0xfc,0x07,0xf8
-,0x00,0x00,0x1f,0xf0,0x7f,0x0f,0xc0
-,0x00,0x00,0x0f,0xe0,0x7f,0xff,0xe0
-,0x00,0x00,0x07,0xc0,0xff,0xff,0xe0
-,0x00,0x00,0x00,0x00,0x7f,0xff,0xe0
-,0x00,0x00,0x00,0x00,0x3f,0xff,0x80
-,0x00,0x00,0x00,0x00,0x1f,0xbf,0x00
-,0x00,0x00,0x00,0x00,0x03,0x18,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-};
-#define Wifi_connected_width 63
-#define Wifi_connected_height 49
-const uint8_t PROGMEM Wifi_connected_bits[] = {
-  0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x03,0xff,0xff,0x80,0x00,0x00
-,0x00,0x00,0x3f,0xff,0xff,0xf8,0x00,0x00
-,0x00,0x01,0xff,0xff,0xff,0xff,0x00,0x00
-,0x00,0x0f,0xff,0xff,0xff,0xff,0xe0,0x00
-,0x00,0x3f,0xff,0xc0,0x07,0xff,0xf8,0x00
-,0x00,0xff,0xf8,0x00,0x00,0x3f,0xfe,0x00
-,0x03,0xff,0x80,0x00,0x00,0x03,0xff,0x80
-,0x07,0xfe,0x00,0x00,0x00,0x00,0xff,0xc0
-,0x1f,0xf8,0x00,0x00,0x00,0x00,0x3f,0xf0
-,0x3f,0xe0,0x01,0xff,0xff,0x00,0x0f,0xf8
-,0x7f,0x80,0x0f,0xff,0xff,0xe0,0x03,0xfc
-,0xff,0x00,0x7f,0xff,0xff,0xfc,0x01,0xfe
-,0xfc,0x01,0xff,0xff,0xff,0xff,0x00,0x7e
-,0x78,0x07,0xff,0xc0,0x07,0xff,0xc0,0x3c
-,0x00,0x0f,0xfc,0x00,0x00,0x7f,0xe0,0x00
-,0x00,0x1f,0xf0,0x00,0x00,0x1f,0xf0,0x00
-,0x00,0x3f,0xc0,0x00,0x00,0x07,0xf8,0x00
-,0x00,0x7f,0x00,0x01,0x00,0x01,0xfc,0x00
-,0x00,0x7e,0x00,0x7f,0xfc,0x00,0xfc,0x00
-,0x00,0x3c,0x03,0xff,0xff,0x80,0x78,0x00
-,0x00,0x00,0x07,0xff,0xff,0xc0,0x00,0x00
-,0x00,0x00,0x1f,0xff,0xff,0xf0,0x00,0x00
-,0x00,0x00,0x3f,0xf0,0x1f,0xf8,0x00,0x00
-,0x00,0x00,0x3f,0x80,0x03,0xf8,0x00,0x00
-,0x00,0x00,0x3f,0x00,0x01,0xf8,0x00,0x00
-,0x00,0x00,0x1c,0x00,0x00,0x70,0x00,0x00
-,0x00,0x00,0x00,0x01,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x0f,0xe0,0x00,0x00,0x00
-,0x00,0x00,0x00,0x1f,0xf0,0x00,0x00,0x00
-,0x00,0x00,0x00,0x3f,0xf8,0x00,0x00,0x00
-,0x00,0x00,0x00,0x3f,0xf8,0x00,0x00,0x00
-,0x00,0x00,0x00,0x3f,0xf8,0x00,0x00,0x00
-,0x00,0x00,0x00,0x3f,0xf8,0x00,0x00,0x00
-,0x00,0x00,0x00,0x1f,0xf0,0x00,0x00,0x00
-,0x00,0x00,0x00,0x0f,0xe0,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00
-};
-//************************************************************************
 void setup() {
   delay(1000);
   Serial.begin(115200);
   SPI.begin();  // Init SPI bus
   mfrc522.PCD_Init(); // Init MFRC522 card
   //-----------initiate OLED display-------------
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-  // you can delet these three lines if you don't want to get the Adfruit logo appear
+{
+    Config_Init();
+    LCD_Init();
+
+    LCD_SetBacklight(100);
+}
+  //-----------initiate OLED display End-------------
+
   display.display();
   delay(2000); // Pause for 2 seconds
   display.clearDisplay();
   //---------------------------------------------
-  connectToWiFi();
+  // Initialize Ethernet or other network connection here if needed
   //---------------------------------------------
+{
+  Serial.begin(115200);
+  WiFi.onEvent(onEvent);
+  ETH.begin(ETH_TYPE, ETH_ADDR, ETH_MDC_PIN, ETH_MDIO_PIN, ETH_POWER_PIN, ETH_CLK_MODE);
+}
+
   configTime(timezone, time_dst, "pool.ntp.org","time.nist.gov");
 }
 //************************************************************************
 void loop() {
-  //check if there's a connection to Wi-Fi or not
-  if(!WiFi.isConnected()){
-    connectToWiFi();    //Retry to connect to Wi-Fi
-  }
+  // Main loop code
+  // Ensure network connectivity if necessary
   //---------------------------------------------
   if (millis() - previousMillis1 >= 1000) {
     previousMillis1 = millis();
     display.clearDisplay();
-    
+
     time_t now = time(nullptr);
     struct tm* p_tm = localtime(&now);
-    display.setTextSize(1);             // Normal 2:2 pixel scale
-    display.setTextColor(WHITE);        // Draw white text
-    display.setCursor(10,0);
-    Serial.println(p_tm);
-    display.setTextSize(4);             // Normal 2:2 pixel scale
-    display.setTextColor(WHITE);        // Draw white text
-    display.setCursor(0,21);
-    if ((p_tm->tm_hour)<10) {display.print("0");display.print(p_tm->tm_hour);}
-    else display.print(p_tm->tm_hour);
-    display.print(":");
-    if ((p_tm->tm_min)<10) {display.print("0");display.println(p_tm->tm_min);}
-    else display.println(p_tm->tm_min);
-    display.display();
+    displayTime(p_tm);
   }
   //---------------------------------------------
   if (millis() - previousMillis2 >= 15000) {
@@ -202,136 +157,13 @@ void loop() {
   }
   delay(50);
   //---------------------------------------------
-  //look for new card
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    return;//got to start of loop if there is no card present
-  }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    return;//if read card serial(0) returns 1, the uid struct contians the ID of the read card.
-  }
-  String CardID ="";
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    CardID += mfrc522.uid.uidByte[i];
-  }
-  //---------------------------------------------
-  if( CardID == OldCardID ){
-    return;
-  }
-  else{
-    OldCardID = CardID;
-  }
-  //---------------------------------------------
-//  Serial.println(CardID);
-  SendCardID(CardID);
-  delay(1000);
-  display.clearDisplay();
-}
-//************send the Card UID to the website*************
-void SendCardID( String Card_uid ){
-  Serial.println("Sending the Card ID");
-  if(WiFi.isConnected()){
-    HTTPClient http;    //Declare object of class HTTPClient
-    //GET Data
-    getData = "?card_uid=" + String(Card_uid) + "&device_token=" + String(device_token); // Add the Card ID to the GET array in order to send it
-    //GET methode
-    Link = URL + getData;
-    http.begin(Link); //initiate HTTP request   //Specify content-type header
-    
-    int httpCode = http.GET();   //Send the request
-    String payload = http.getString();    //Get the response payload
+  checkForNewCard();
 
-//    Serial.println(Link);   //Print HTTP return code
-    Serial.println(httpCode);   //Print HTTP return code
-    Serial.println(Card_uid);     //Print Card ID
-    Serial.println(payload);    //Print request response payload
-
-    if (httpCode == 200) {
-      if (payload.substring(0, 5) == "login") {
-        String user_name = payload.substring(5);
-    //  Serial.println(user_name);
-        
-        display.clearDisplay();
-        display.setTextSize(2);             // Normal 2:2 pixel scale
-        display.setTextColor(WHITE);        // Draw white text
-        display.setCursor(15,0);             // Start at top-left corner
-        display.print(F("Welcome"));
-        display.setCursor(0,20);
-        display.print(user_name);
-        display.display();
-      }
-      else if (payload.substring(0, 6) == "logout") {
-        String user_name = payload.substring(6);
-    //  Serial.println(user_name);
-        
-        display.clearDisplay();
-        display.setTextSize(2);             // Normal 2:2 pixel scale
-        display.setTextColor(WHITE);        // Draw white text
-        display.setCursor(10,0);             // Start at top-left corner
-        display.print(F("Good Bye"));
-        display.setCursor(0,20);
-        display.print(user_name);
-        display.display();
-      }
-      else if (payload == "succesful") {
-        display.clearDisplay();
-        display.setTextSize(2);             // Normal 2:2 pixel scale
-        display.setTextColor(WHITE);        // Draw white text
-        display.setCursor(5,0);             // Start at top-left corner
-        display.print(F("New Card"));
-        display.display();
-      }
-      else if (payload == "available") {
-        display.clearDisplay();
-        display.setTextSize(2);             // Normal 2:2 pixel scale
-        display.setTextColor(WHITE);        // Draw white text
-        display.setCursor(5,0);             // Start at top-left corner
-        display.print(F("Free Card"));
-        display.display();
-      }
-      delay(100);
-      http.end();  //Close connection
-    }
-  }
 }
-//********************connect to the WiFi******************
-void connectToWiFi(){
-    WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
-    delay(1000);
-    WiFi.mode(WIFI_STA);
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, password);
+//************************************************************************
 
-    display.clearDisplay();
-    display.setTextSize(1);             // Normal 1:1 pixel scale
-    display.setTextColor(WHITE);        // Draw white text
-    display.setCursor(0, 0);             // Start at top-left corner
-    display.print(F("Connecting to \n"));
-    display.setCursor(0, 50);   
-    display.setTextSize(2);          
-    display.print(ssid);
-    display.drawBitmap( 73, 10, Wifi_start_bits, Wifi_start_width, Wifi_start_height, WHITE);
-    display.display();
-    
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("Connected");
-    
-    display.clearDisplay();
-    display.setTextSize(2);             // Normal 1:1 pixel scale
-    display.setTextColor(WHITE);        // Draw white text
-    display.setCursor(8, 0);             // Start at top-left corner
-    display.print(F("Connected \n"));
-    display.drawBitmap( 33, 15, Wifi_connected_bits, Wifi_connected_width, Wifi_connected_height, WHITE);
-    display.display();
-    
-    Serial.print("IP address: ");
-    Serial.println(WiFi.localIP());  //IP address assigned to your ESP
-    
-    delay(1000);
-}
+// Function definitions for displayTime, checkForNewCard, SendCardID, etc.
+
+//************************************************************************
+// Add any necessary functions here, such as for handling Ethernet connectivity
 //=======================================================================
